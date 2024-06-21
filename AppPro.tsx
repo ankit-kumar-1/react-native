8c643723fe2a29fd2da9984b88b3aeb147e91da4
ankitverma242001@gmail.com <ankitverma242001@gmail.com>
2024-06-21 16:05:40 +0530

    first commit

---
+++ AppPro.tsx
@@ -0,0 +1,29 @@
+/* eslint-disable prettier/prettier */
+import React from 'react';
+import {View, Text, StyleSheet, useColorScheme} from 'react-native';
+
+function AppPro(){
+    const isDarkMode = useColorScheme() === 'dark';
+    return (
+        <View style={styles.container}>
+            <Text style={isDarkMode ? styles.whiteText : styles.whiteText}>Hello</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container : {
+        flex : 1,
+        alignItems: 'center',
+        justifyContent:'center',
+    },
+    whiteText: {
+        color: '#FFFFFF',
+    },
+    darkText: {
+        color: '#000000',
+    },
+});
+
+export default AppPro;
+
--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+/* eslint-disable prettier/prettier */
 /**
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
+import AppPro from './AppPro';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => AppPro);
